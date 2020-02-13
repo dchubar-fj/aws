fea703d06a969a40cee3ce615ff5318130d5695b
Dmitry <dmitry.chubar@fivejars.com>
2020-02-13 15:13:24 +0300

    Check repos and packages before install

--- php73-apcu.sh
+++ php73-apcu.sh
@@ -1,9 +1,38 @@
 #!/bin/sh
 set -e
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install -y https://rpmfind.net/linux/centos/7.7.1908/os/x86_64/Packages/scl-utils-20130529-19.el7.x86_64.rpm
-yum install -y https://rpmfind.net/linux/centos/7.7.1908/os/x86_64/Packages/libedit-3.0-12.20121213cvs.el7.x86_64.rpm
-yum-config-manager --enable epel
-yum-config-manager --enable remi-php73
-yum install -y php73-php-pecl-apcu
+
+if ! yum repolist | grep 'epel'
+then
+  yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  yum-config-manager --enable epel
+fi
+
+if ! yum repolist | grep 'remi'
+then
+  yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+  yum-config-manager --enable remi-php73
+fi
+
+if ! is_installed scl-utils;
+then
+  yum install -y https://rpmfind.net/linux/centos/7.7.1908/os/x86_64/Packages/scl-utils-20130529-19.el7.x86_64.rpm
+fi
+
+if ! is_installed libedit;
+then
+  yum install -y https://rpmfind.net/linux/centos/7.7.1908/os/x86_64/Packages/libedit-3.0-12.20121213cvs.el7.x86_64.rpm
+fi
+
+if ! is_installed php73-php-pecl-apcu;
+then
+  yum install -y php73-php-pecl-apcu
+fi
+
+#-------------------------- Functions --------------------------------
+is_installed() {
+  if yum list installed "$@" >/dev/null 2>&1; then
+    true
+  else
+    false
+  fi
+}
